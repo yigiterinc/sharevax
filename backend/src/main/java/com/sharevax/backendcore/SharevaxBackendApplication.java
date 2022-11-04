461e846e7f610ffbf081a7ad96d09ab5d2fd5826
Yigit Kemal Erinc <erincyigit@gmail.com>
2022-11-04 14:14:33 +0300

    feat: init backend

---
+++ backend/src/main/java/com/sharevax/backendcore/SharevaxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sharevax.backendcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharevaxBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharevaxBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/sharevax/backendcore/SharevaxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharevax.backendcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharevaxBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
