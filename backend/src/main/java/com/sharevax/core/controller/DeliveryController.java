057b092f8a9ae4ad8a7fbd05012b08ad2d750394
Jingyi Jia <alley53745968@gmail.com>
2022-12-01 15:36:32 +0100

    add maven dependencies. create DeliveryController/Service, create TestController

---
+++ backend/src/main/java/com/sharevax/core/controller/DeliveryController.java
@@ -0,0 +1,42 @@
+package com.sharevax.core.controller;
+
+import com.sharevax.core.model.Delivery;
+import com.sharevax.core.service.DeliveryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/deliveries")
+public class DeliveryController {
+    private final DeliveryService deliveryService;
+
+    public DeliveryController(DeliveryService deliveryService) {
+        this.deliveryService = deliveryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Delivery>> getAllRunningDeliveries() {
+        return ResponseEntity.ok(deliveryService.getAllRunningDeliveries());
+    }
+
+    @GetMapping
+    @RequestMapping("/UID/{id}")
+    public ResponseEntity<Delivery> getDeliveryById(@PathVariable Integer id) {
+        return ResponseEntity.ok(deliveryService.getDeliveryById(id));
+    }
+
+    @GetMapping
+    @RequestMapping("/country/{country_id}")
+    public ResponseEntity<List<Delivery>> getDeliveriesByCountryID(@PathVariable Integer country_id) {
+        return ResponseEntity.ok(deliveryService.getDeliveriesByCountryID(country_id));
+    }
+
+    @GetMapping
+    @RequestMapping("/matching}")
+    public ResponseEntity<String> matching() {
+        deliveryService.matching();
+        return ResponseEntity.ok("Matching completed");
+    }
+}
