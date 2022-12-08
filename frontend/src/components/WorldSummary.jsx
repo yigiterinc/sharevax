908a05c4adf3fa46bb576e70900aaa1feb050151
Rui Wang <4a9ebb44@gmail.com>
2022-12-08 18:27:59 +0100

    add `WorldSummary` component placeholder

---
+++ frontend/src/components/WorldSummary.jsx
@@ -0,0 +1,6 @@
+//World Summary table on the right of the map on Home page
+//import React from 'react';
+
+export default function WorldSummary() {
+	return <div>WorldSummary</div>;
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import OverviewMap from '../components/OverviewMap';
 import OverviewTable from '../components/OverviewTable';
 import Demand from '../components/Demand';
 import Supply from '../components/Supply';
+import WorldSummary from '../components/WorldSummary';
 function Home() {
 	return (
 		<div className='flex flex-col items-center gap-12 m-6 grow'>
@@ -10,6 +11,7 @@ function Home() {
 			<OverviewTable />
 			<Demand />
 			<Supply />
+			<WorldSummary />
 		</div>
 	);
 }
