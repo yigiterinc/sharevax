6f526af5cde7f4503ea923fee6cca1fe7be382ac
Rui Wang <4a9ebb44@gmail.com>
2022-12-08 18:48:24 +0100

    correct file names

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,6 @@
+//headers on each page displaying page title such as 'Home' and country/icon selected.
+import React from 'react';
+
+export default function Header() {
+	return <div>Header</div>;
+}
