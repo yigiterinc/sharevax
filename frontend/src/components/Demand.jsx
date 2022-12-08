d7c463a7c3c2c6fffc0083285292f047721b02fb
Rui Wang <4a9ebb44@gmail.com>
2022-12-08 20:10:57 +0100

    update `vaccineType` in `Demand, `Supply`

--- frontend/src/components/Demand.jsx
+++ frontend/src/components/Demand.jsx
@@ -29,6 +29,7 @@ export default function Demand() {
 		event.preventDefault();
 		console.log(formValues);
 	};
+
 	return (
 		<div>
 			<form onSubmit={handleSubmit}>
@@ -43,16 +44,61 @@ export default function Demand() {
 							onChange={handleInputChange}
 						/>
 					</Grid>
+
 					<Grid item>
-						<TextField
-							id='vaccineType-input'
-							name='vaccineType'
-							label='Vaccine Type'
-							type='text'
-							value={formValues.vaccineType}
-							onChange={handleInputChange}
-						/>
+						<FormControl>
+							<FormLabel className='mt-3'>Vaccine Type</FormLabel>
+							<RadioGroup
+								name='vaccineType'
+								value={formValues.vaccineType}
+								onChange={handleInputChange}
+								column
+								className='my-2'
+							>
+								<Grid item>
+									<FormControlLabel key='BIONTECH' value='BIONTECH' control={<Radio size='small' />} label='BIONTECH' />
+									<FormControlLabel key='MODERNA' value='MODERNA' control={<Radio size='small' />} label='MODERNA' />
+									<FormControlLabel
+										key='ASTRAZENECA'
+										value='ASTRAZENECA'
+										control={<Radio size='small' />}
+										label='ASTRAZENECA'
+									/>
+								</Grid>
+								<Grid>
+									<FormControlLabel key='PFIZERL' value='PFIZER' control={<Radio size='small' />} label='PFIZER' />
+									<FormControlLabel key='JANSSEN' value='JANSSEN' control={<Radio size='small' />} label='JANSSEN' />
+									<FormControlLabel
+										key='SINOPHARM'
+										value='SINOPHARM'
+										control={<Radio size='small' />}
+										label='SINOPHARM'
+									/>
+								</Grid>
+								<Grid>
+									<FormControlLabel key='SINOVAC' value='SINOVAC' control={<Radio size='small' />} label='SINOVAC' />
+									<FormControlLabel
+										key='SPUTNIK_V'
+										value='SPUTNIK_V'
+										control={<Radio size='small' />}
+										label='SPUTNIK_V'
+									/>
+									<FormControlLabel key='COVAXIN' value='COVAXIN' control={<Radio size='small' />} label='COVAXIN' />
+								</Grid>
+								<Grid>
+									<FormControlLabel
+										key='COVISHIELD'
+										value='COVISHIELD'
+										control={<Radio size='small' />}
+										label='COVISHIELD'
+									/>
+									<FormControlLabel key='NOVAVAX' value='NOVAVAX' control={<Radio size='small' />} label='NOVAVAX' />
+									<FormControlLabel key='CANSINO' value='CANSINO' control={<Radio size='small' />} label='CANSINO' />
+								</Grid>
+							</RadioGroup>
+						</FormControl>
 					</Grid>
+
 					<Grid item>
 						<TextField
 							id='quantity-input'
@@ -63,6 +109,7 @@ export default function Demand() {
 							onChange={handleInputChange}
 						/>
 					</Grid>
+
 					<Grid item>
 						<FormControl>
 							<FormLabel className='mt-3'>Urgency</FormLabel>
@@ -79,6 +126,7 @@ export default function Demand() {
 							</RadioGroup>
 						</FormControl>
 					</Grid>
+
 					<Button variant='contained' color='black' type='submit'>
 						Submit
 					</Button>
--- frontend/src/components/Supply.jsx
+++ frontend/src/components/Supply.jsx
@@ -3,6 +3,11 @@ import {useState} from 'react';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Radio from '@material-ui/core/Radio';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import RadioGroup from '@material-ui/core/RadioGroup';
 const defaultValues = {
 	country: '',
 	vaccineType: '',
@@ -38,16 +43,61 @@ export default function Supply() {
 							onChange={handleInputChange}
 						/>
 					</Grid>
+
 					<Grid item>
-						<TextField
-							id='vaccineType-input'
-							name='vaccineType'
-							label='Vaccine Type'
-							type='text'
-							value={formValues.vaccineType}
-							onChange={handleInputChange}
-						/>
+						<FormControl>
+							<FormLabel className='mt-3'>Vaccine Type</FormLabel>
+							<RadioGroup
+								name='vaccineType'
+								value={formValues.vaccineType}
+								onChange={handleInputChange}
+								column
+								className='my-2'
+							>
+								<Grid item>
+									<FormControlLabel key='BIONTECH' value='BIONTECH' control={<Radio size='small' />} label='BIONTECH' />
+									<FormControlLabel key='MODERNA' value='MODERNA' control={<Radio size='small' />} label='MODERNA' />
+									<FormControlLabel
+										key='ASTRAZENECA'
+										value='ASTRAZENECA'
+										control={<Radio size='small' />}
+										label='ASTRAZENECA'
+									/>
+								</Grid>
+								<Grid>
+									<FormControlLabel key='PFIZERL' value='PFIZER' control={<Radio size='small' />} label='PFIZER' />
+									<FormControlLabel key='JANSSEN' value='JANSSEN' control={<Radio size='small' />} label='JANSSEN' />
+									<FormControlLabel
+										key='SINOPHARM'
+										value='SINOPHARM'
+										control={<Radio size='small' />}
+										label='SINOPHARM'
+									/>
+								</Grid>
+								<Grid>
+									<FormControlLabel key='SINOVAC' value='SINOVAC' control={<Radio size='small' />} label='SINOVAC' />
+									<FormControlLabel
+										key='SPUTNIK_V'
+										value='SPUTNIK_V'
+										control={<Radio size='small' />}
+										label='SPUTNIK_V'
+									/>
+									<FormControlLabel key='COVAXIN' value='COVAXIN' control={<Radio size='small' />} label='COVAXIN' />
+								</Grid>
+								<Grid>
+									<FormControlLabel
+										key='COVISHIELD'
+										value='COVISHIELD'
+										control={<Radio size='small' />}
+										label='COVISHIELD'
+									/>
+									<FormControlLabel key='NOVAVAX' value='NOVAVAX' control={<Radio size='small' />} label='NOVAVAX' />
+									<FormControlLabel key='CANSINO' value='CANSINO' control={<Radio size='small' />} label='CANSINO' />
+								</Grid>
+							</RadioGroup>
+						</FormControl>
 					</Grid>
+
 					<Grid item>
 						<TextField
 							id='quantity-input'
@@ -58,6 +108,7 @@ export default function Supply() {
 							onChange={handleInputChange}
 						/>
 					</Grid>
+
 					<Grid item>
 						<TextField
 							id='unitPrice-input'
@@ -68,6 +119,7 @@ export default function Supply() {
 							onChange={handleInputChange}
 						/>
 					</Grid>
+
 					<Button variant='contained' color='black' type='submit'>
 						Submit
 					</Button>
