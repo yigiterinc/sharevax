018cdef1452994f0da0d24b258a1b22b12310bfa
Jingyi Jia <alley53745968@gmail.com>
2022-12-08 11:03:13 +0100

    fix bug: cross origin by adding @CrossOrigin and WebConfig

--- backend/src/main/java/com/sharevax/core/controller/DeliveryController.java
+++ backend/src/main/java/com/sharevax/core/controller/DeliveryController.java
@@ -4,12 +4,14 @@ import com.sharevax.core.model.dto.DeliveryDto;
 import com.sharevax.core.service.DeliveryService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/deliveries")
 public class DeliveryController {
--- backend/src/main/java/com/sharevax/core/controller/HarborController.java
+++ backend/src/main/java/com/sharevax/core/controller/HarborController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/harbors")
 public class HarborController {
