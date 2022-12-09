d6274f76dc9206055638ea4a407eb79bc0a059e0
Rui Wang <4a9ebb44@gmail.com>
2022-12-09 23:38:53 +0100

    fix font style & color

--- frontend/src/components/WorldSummary.jsx
+++ frontend/src/components/WorldSummary.jsx
@@ -6,24 +6,24 @@ export default function WorldSummary() {
 		<div>
 			<div className='mx-5 mt-3 grid grid-col-1 gap-2'>
 				<div className='grid grid-rows-2 grid-flow-col bg-main-100/100 rounded-lg mb-6 p-3'>
-					<div className='font-bold font-mono'>Current Transport</div>
-					<div className='font-bold mt-5'>324</div>
+					<div className='font-bold text-white'>Current Transport</div>
+					<div className='font-bold mt-5 text-white'>324</div>
 					<div className='row-span-2'>
-						<GiShipBow size={70} />
+						<GiShipBow size={70} color={'white'} />
 					</div>
 				</div>
 				<div className='grid grid-rows-2 grid-flow-col bg-main-100/75 rounded-lg mb-6 p-3'>
-					<div className='font-bold font-mono'>Daily Vaccine Production</div>
-					<div className='font-bold mt-5'>12345</div>
+					<div className='font-bold text-white'>Daily Vaccine Production</div>
+					<div className='font-bold mt-5 text-white'>12345</div>
 					<div className='row-span-2'>
-						<GiRobotGrab size={70} />
+						<GiRobotGrab size={70} color={'white'} />
 					</div>
 				</div>
 				<div className='grid grid-rows-2 grid-flow-col bg-main-100/50 rounded-lg p-3'>
-					<div className='font-bold font-mono'>Daily Vaccine Consumption</div>
-					<div className='font-bold mt-5'>12314</div>
+					<div className='font-bold text-white'>Daily Vaccine Consumption</div>
+					<div className='font-bold mt-5 text-white'>12314</div>
 					<div className='row-span-2'>
-						<TbVaccine size={70} />
+						<TbVaccine size={70} color={'white'} />
 					</div>
 				</div>
 			</div>
