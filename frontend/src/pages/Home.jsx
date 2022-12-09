e2f5f8ea44dd232298bcaf27e24a989da54037eb
Gülbike İmge Köksal <koksalg15@itu.edu.tr>
2022-12-09 02:01:21 +0100

    style: country dropdown

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,7 +6,9 @@ import CountryDropdown from '../components/CountryDropdown';
 function Home() {
 	return (
 		<div className='flex flex-col items-center gap-12 m-6 grow'>
-			<CountryDropdown />
+			<div className='flex justify-end w-full -mb-8'>
+				<CountryDropdown />
+			</div>
 			<OverviewMap />
 			<OverviewTable />
 		</div>
