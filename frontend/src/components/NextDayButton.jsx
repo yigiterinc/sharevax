a1bf88dac4318142571782912df2b625129f7817
Gülbike İmge Köksal <koksalg15@itu.edu.tr>
2022-12-09 02:37:05 +0100

    style: add color theme palette

--- frontend/src/components/NextDayButton.jsx
+++ frontend/src/components/NextDayButton.jsx
@@ -2,7 +2,7 @@ function NextDayButton() {
 	return (
 		<button
 			type='button'
-			className='text-white bg-[#00a7db] hover:bg-[#009aca] active:ring-4 active:ring-[#64daff] font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2'
+			className='text-white bg-main-100 hover:bg-main-200 active:ring-4 active:ring-main-50 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2'
 		>
 			Next Day
 		</button>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,21 @@
 /* eslint-env node */
 /** @type {import('tailwindcss').Config} */
+
+const themeColors = {
+	main: {
+		50: '#64daff',
+		100: '#008db9',
+		200: '#0080a8',
+	},
+};
+
 module.exports = {
 	content: ['./src/**/*.{html,js,jsx}'],
 	important: '#root',
 	theme: {
-		extend: {},
+		extend: {
+			colors: {...themeColors},
+		},
 	},
 	plugins: [],
 };
