c48946c12a8aa9309a638d45d45dcc68952e1413
Rui Wang <4a9ebb44@gmail.com>
2022-12-10 10:02:31 +0100

    add `ACTIVE_DELIVERIES`

--- frontend/src/services/services.js
+++ frontend/src/services/services.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {COUNTRIES, SUPPLIES, HARBORS, PROGRESS_SIMULATION} from './endpoints';
+import {COUNTRIES, SUPPLIES, HARBORS, ACTIVE_DELIVERIES, PROGRESS_SIMULATION} from './endpoints';
 
 export const fetchCountries = async function () {
 	return await axios.get(COUNTRIES);
@@ -11,6 +11,11 @@ export const fetchSupplies = async function () {
 
 export const fetchHarbors = async function () {
 	return await axios.get(HARBORS);
+};
+
+export const fetchActiveDeliveries = async function () {
+	const url = ACTIVE_DELIVERIES;
+	return await axios.get(url);
 };
 
 export const progressSimulation = async function () {
