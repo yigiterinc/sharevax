a5e38630c712f42b4fcf693ff540f9b4ece36dfa
Gülbike İmge Köksal <koksalg15@itu.edu.tr>
2022-12-10 17:52:28 +0100

    Auto stash before revert of "feat: add function to separate number with commas"

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -22,3 +22,32 @@ export const getColor = (val) => {
 	console.log(color);
 	return colors[colors.length - 1];
 };
+
+export const separateNumberWithCommas = (val) => {
+	// remove sign if negative
+	var sign = 1;
+	if (val < 0) {
+		sign = -1;
+		val = -val;
+	}
+	// trim the number decimal point if it exists
+	let num = val.toString().includes('.') ? val.toString().split('.')[0] : val.toString();
+	let len = num.toString().length;
+	let result = '';
+	let count = 1;
+
+	for (let i = len - 1; i >= 0; i--) {
+		result = num.toString()[i] + result;
+		if (count % 3 === 0 && count !== 0 && i !== 0) {
+			result = ',' + result;
+		}
+		count++;
+	}
+
+	// add number after decimal point
+	if (val.toString().includes('.')) {
+		result = result + '.' + val.toString().split('.')[1];
+	}
+	// return result with - sign if negative
+	return sign < 0 ? '-' + result : result;
+};
