84d2400c639b26d133d59677b180c43a29340912
Gülbike İmge Köksal <koksalg15@itu.edu.tr>
2022-12-14 03:41:51 +0100

    fix: legend width

--- frontend/src/components/Legend.jsx
+++ frontend/src/components/Legend.jsx
@@ -1,7 +1,7 @@
 // Legend for explaining the color & percentage relationship, specified in OverviewMap.jsx
 const Legend = ({legendItems}) => {
 	return (
-		<div className='flex flex-col justify-center px-1 w-1/4 h-16 absolute bottom-1 left-1 z-50 bg-white bg-opacity-70 rounded-md'>
+		<div className='flex flex-col justify-center px-1 w-64 h-16 absolute bottom-1 left-1 z-50 bg-white bg-opacity-70 rounded-md'>
 			<div className='flex'>Vaccination Rates</div>
 			<div className='flex w-full'>
 				{legendItems.map((item) => (
