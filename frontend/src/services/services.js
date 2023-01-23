caae2d5ca6961e9ceda4d719546007397363d054
Gülbike İmge Köksal <koksalg15@itu.edu.tr>
2023-01-24 00:32:03 +0300

    feat: add create event endpoint

--- frontend/src/services/services.js
+++ frontend/src/services/services.js
@@ -1,5 +1,13 @@
 import axios from 'axios';
-import {COUNTRIES, SUPPLIES, HARBORS, ACTIVE_DELIVERIES, PROGRESS_SIMULATION, SIMULATION_DAY} from './endpoints';
+import {
+	COUNTRIES,
+	SUPPLIES,
+	HARBORS,
+	ACTIVE_DELIVERIES,
+	PROGRESS_SIMULATION,
+	SIMULATION_DAY,
+	CREATE_EVENT,
+} from './endpoints';
 
 export const fetchCountries = async function () {
 	return await axios.get(COUNTRIES);
@@ -24,4 +32,8 @@ export const progressSimulation = async function () {
 
 export const fetchSimulationDay = async function () {
 	return await axios.get(SIMULATION_DAY);
+};
+
+export const createEvent = async function (type, subject, remainingDaysToStart) {
+	return await axios.post(CREATE_EVENT, {type, subject, remainingDaysToStart});
 };
