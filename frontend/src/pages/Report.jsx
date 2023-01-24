e46b44d9229104966f312f79e944677d2c7bf0d3
Rui Wang <4a9ebb44@gmail.com>
2023-01-24 23:40:14 +0100

    show Suggestion in Report page atm

--- frontend/src/pages/Report.jsx
+++ frontend/src/pages/Report.jsx
@@ -1,6 +1,12 @@
 //Report Page
+import Suggestion from '../components/Suggestion';
 function Report() {
-	return <div className='flex flex-col items-center gap-12'>Report page</div>;
+	return (
+		<div className='flex flex-col items-center gap-12'>
+			Report page
+			<Suggestion />
+		</div>
+	);
 }
 
 export default Report;
