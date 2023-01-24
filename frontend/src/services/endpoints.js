3f4c71e0a960a534dae64babf68e6de8293ba3a2
Rui Wang <4a9ebb44@gmail.com>
2023-01-24 23:41:08 +0100

    add suggestion endpoint

--- frontend/src/services/endpoints.js
+++ frontend/src/services/endpoints.js
@@ -7,5 +7,6 @@ export const DELIVERIES = `${BASE_URL}/deliveries`;
 export const ACTIVE_DELIVERIES = `${BASE_URL}/deliveries/active`;
 export const CREATE_SUPPLY = `${BASE_URL}/supplies`;
 export const CREATE_DEMAND = `${BASE_URL}/demands`;
+export const SUGGESTION = `${BASE_URL}/suggestion`;
 export const PROGRESS_SIMULATION = `${BASE_URL}/simulation/increment-day`;
 export const SIMULATION_DAY = `${BASE_URL}/simulation/current-day`;
