b559ad203c433fd7ea22bbb9a057b543c65660be
Rui Wang <4a9ebb44@gmail.com>
2023-01-27 15:52:37 +0100

    clean up

--- frontend/src/pages/Report.jsx
+++ frontend/src/pages/Report.jsx
@@ -1,12 +1,6 @@
 //Report Page
-import Suggestion from '../components/Suggestion';
 function Report() {
-	return (
-		<div className='flex flex-col items-center gap-12'>
-			Report page
-			<Suggestion />
-		</div>
-	);
+	return <div className='flex flex-col items-center gap-12'>Report page</div>;
 }
 
 export default Report;
--- frontend/src/pages/Suggestion.jsx
+++
@@ -1,4 +0,0 @@
-//Suggestion Page
-export default function Suggestion() {
-	return <div className='flex flex-col items-center gap-12'>Suggestion page</div>;
-}
