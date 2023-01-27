c5c86bcbb5e49306fd996714fb74026c5794f31b
Rui Wang <4a9ebb44@gmail.com>
2023-01-27 15:52:20 +0100

    fix: link SuggestionPage

---
+++ frontend/src/pages/SuggestionPage.jsx
@@ -0,0 +1,9 @@
+//Suggestion Page
+import Suggestion from '../components/Suggestion';
+export default function SuggestionPage() {
+	return (
+		<div className='flex flex-col items-center gap-12'>
+			<Suggestion />
+		</div>
+	);
+}
